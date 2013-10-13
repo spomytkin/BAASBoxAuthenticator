48024a70cd011d4ac9eed028d24a2484a27c4ddd
Sergey Pomytkin <spomytkin@gmail.com>
2013-10-13 10:27:49 -0400

    Update BAASBoxServerAuthenticateServices.java
    refactor from get/set to result

--- src/com/baasbox/android/authentication/BAASBoxServerAuthenticateServices.java
+++ src/com/baasbox/android/authentication/BAASBoxServerAuthenticateServices.java
@@ -8,10 +8,11 @@ import android.content.Context;
 import com.baasbox.android.BAASBox;
 import com.baasbox.android.BAASBoxClientException;
 import com.baasbox.android.BAASBoxConfig;
-import com.baasbox.android.BAASBoxException;
+import com.baasbox.android.BAASBoxConfig.AuthType;
+import com.baasbox.android.BAASBoxConnectionException;
+import com.baasbox.android.BAASBoxInvalidSessionException;
 import com.baasbox.android.BAASBoxResult;
 import com.baasbox.android.BAASBoxServerException;
-import com.baasbox.android.BAASBoxConfig.AuthType;
 
 /**
  * BAASBox calls 
@@ -23,13 +24,13 @@ public class BAASBoxServerAuthenticateServices {
 
 	public BAASBoxServerAuthenticateServices(Context ctx ){
 		BAASBoxConfig config = new BAASBoxConfig();
-		//config.API_DOMAIN = "10.0.2.2";//TODO change to real IP for device or remote server test
-		config.API_DOMAIN =  "192.168.0.105";
+		config.API_DOMAIN = "10.0.2.2";//TODO change to real IP for device or remote server test. e.g.:
+		//config.API_DOMAIN =  "192.168.0.105";
 		config.AUTHENTICATION_TYPE = AuthType.SESSION_TOKEN;	
 		bbox = new BAASBox(config, ctx);
 	}
 	
-    public String userSignUp(String username, String email, String password, String authType) throws Exception {
+    public String userSignUp(String username, String email, String password, String authType) throws BAASBoxClientException, BAASBoxServerException, BAASBoxInvalidSessionException, BAASBoxConnectionException {
 
 		JSONObject user = new JSONObject();
 		JSONObject visibleByTheUser = new JSONObject();
@@ -43,32 +44,14 @@ public class BAASBoxServerAuthenticateServices {
 			throw new Error(e);
 		}
 		
-		BAASBoxResult<Void> result = bbox.signup(user);
-		try {
-			result.get();
-			
-		} catch (BAASBoxClientException e) {
-			if (e.httpStatus == 400) {
-				// "Signup failed");
- //TODO handle
-			}
-		} catch (BAASBoxServerException e) {
-			 //TODO handle
-		} catch (BAASBoxException e) {
-			 //TODO handle
-		}
-
-
-        return bbox.getSessionToken();
+		BAASBoxResult<String> result = bbox.signup(user);
+		return result.get();
+  
         
     }
 
     public String userSignIn(String username, String password) throws Exception {
-    	BAASBoxResult<Void> rez = bbox.login(username, password);
-    	rez.get();
-        return  bbox.getSessionToken();
+    	BAASBoxResult<String> rez = bbox.login(username, password);   	
+        return  rez.get();
     }
-
-
-
 }
